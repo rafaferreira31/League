c5387161dbe951a7716d7bc05e95f2168db050cc
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-09-09 21:18:11 +0100

    Add project files.

---
+++ League/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace League.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
