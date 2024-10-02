8ca45f3dad602925520b82b18e343f02c0d8e5b7
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-10-02 16:12:29 +0100

    Error 404 View

--- League/Program.cs
+++ League/Program.cs
@@ -20,15 +20,20 @@ namespace League
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Errors/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
